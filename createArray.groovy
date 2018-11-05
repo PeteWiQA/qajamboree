1d464b5c552f0a98c822ccddca6981c4a80218f2
DonPedroQA <44758394+DonPedroQA@users.noreply.github.com>
2018-11-05 13:16:16 -0500

    Create createArray.groovy

---
+++ createArray.groovy
@@ -0,0 +1,22 @@
+//Create the List
+monthList = []
+
+//List of incomes that are $1 over the threshold
+//The '' for the first value is empty so the Array values start at position 1. Easy to handle in loops
+grossIncome48=['',24281,32921,41561,50201,58841,67481,76121,84761]
+
+//A list of items
+item1 = 'January'
+item2 = 'February'
+item3 = 'March'
+
+//Add item to the List using a standard 'push' method
+monthList.push(item1)
+monthList.push(item2)
+monthList.push(item3)
+
+//Print the entire List
+println(monthList[])
+
+//Print an item from the List
+println(monthList[1])
