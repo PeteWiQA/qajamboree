d6a3577a134cc5633ad2d077ce96390689af70a7
DonPedroQA <44758394+DonPedroQA@users.noreply.github.com>
2018-11-05 13:43:19 -0500

    Create splitAndSubstring.groovy

---
+++ Groovy/splitAndSubstring.groovy
@@ -0,0 +1,7 @@
+String[] parsedDate = importDate.split('- ')[0]
+//Similar to the Split of Javascript, 0 and 1 can be used. 0 is text up to the delimeter. 1 is the text after delimeter
+
+//First digit is the starting position
+//Second digit is the ending location
+//Using 10,11 is to read 1 character starting at position 10
+monthDay = businessDays.substring(10,11)
