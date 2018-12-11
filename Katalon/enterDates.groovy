ca58525449444d15cf073926341d0bac39b630d5
DonPedroQA <44758394+DonPedroQA@users.noreply.github.com>
2018-12-11 11:10:56 -0500

    Delete enterDates.groovy

--- Katalon/enterDates.groovy
+++
@@ -1,9 +0,0 @@
-//This will get around the problem with the different browsers and a datepicker
-//It uses setText for Firefox and sendKeys for Chrome
-import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
-browserName=DriverFactory.getExecutedBrowser().getName()
- if (browserName=="FIREFOX_DRIVER"){
-	 WebUI.setText(findTestObject('dateField'), '2017-10-31')
- } else {
- 	WebUI.sendKeys(findTestObject('dateField'), Keys.chord('10-31-2017', Keys.ENTER, Keys.TAB))
- }
